0d67a402d8a1d8bdfcdc08a4c5532af6f3a7b5a3
Juan Oh <jioh1982@gmail.com>
2025-05-14 11:50:02 -0400

    first commit

---
+++ Day01/Program.cs
@@ -0,0 +1,94 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine($"Running directory: {Directory.GetCurrentDirectory()}");
+
+string[] input = File.ReadAllLines($"{Directory.GetCurrentDirectory()}/input.txt");
+
+var result = CalculateSum1(input.ToList());
+Console.WriteLine(result);
+
+result = CalculateSum2(input.ToList());
+Console.WriteLine(result);
+
+int CalculateSum1(List<string> lines)
+{
+    if (lines.Count < 1)
+    {
+        return 0;
+    }
+    var line = lines.First();
+    string num = line.FirstOrDefault(c => char.IsDigit(c)).ToString();
+    num += line.LastOrDefault(c => char.IsDigit(c));
+    
+    lines.RemoveAt(0);
+    return int.Parse(num) + CalculateSum1(lines);
+}
+
+int CalculateSum2(List<string> lines)
+{
+    if (lines.Count < 1)
+    {
+        return 0;
+    }
+    var num = GetStringNumber(lines.First(), false) * 10;
+    num += GetStringNumber(lines.First(), true);
+    lines.RemoveAt(0);
+    return num + CalculateSum2(lines);
+}
+
+int GetStringNumber(string line, bool reverse)
+{
+    int num;
+    if (line.Length is 0)
+    {
+        return -1;
+    }
+    var numbers = new Dictionary<string, int>
+    {
+        { "zero", 0 },
+        { "one", 1 },
+        { "two", 2 },
+        { "three", 3 },
+        { "four", 4 },
+        { "five", 5 },
+        { "six", 6 },
+        { "seven", 7 },
+        { "eight", 8 },
+        { "nine", 9 }
+    };
+    if (reverse)
+    {
+        if (char.IsDigit(line[line.Length - 1]))
+        {
+            return int.Parse(line[line.Length - 1].ToString());
+        }
+        num = numbers.Where(pair => line.EndsWith(pair.Key))
+            .FirstOrDefault(defaultValue: new KeyValuePair<string, int>(string.Empty, -1)).Value;
+        if (num is -1)
+        {
+            num = GetStringNumber(line.Remove(line.Length - 1), reverse);
+        }
+        else
+        {
+            return num;
+        }
+    }
+    else
+    {
+        if (char.IsDigit(line[0]))
+        {
+            return int.Parse(line[0].ToString());
+        }
+        num = numbers.Where(pair => line.StartsWith(pair.Key))
+            .FirstOrDefault(defaultValue: new KeyValuePair<string, int>(string.Empty, -1)).Value;
+        if (num is -1)
+        {
+            num = GetStringNumber(line.Substring(1), reverse);
+        }
+        else
+        {
+            return num;
+        }
+    }
+    return num;
+}
---
+++ Day02/Program.cs
@@ -0,0 +1,91 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine($"Running directory: {Directory.GetCurrentDirectory()}");
+
+string[] input = File.ReadAllLines($"{Directory.GetCurrentDirectory()}/input.txt");
+
+//input.ToList().ForEach(Console.WriteLine);
+
+int[] checker = [12, 13, 14];
+
+Console.WriteLine(GetGames());
+Console.WriteLine(GetGames2());
+
+int GetGames()
+{
+    int total = 0;
+    foreach (var (line, index) in input.Select((line, index) => (line, index)))
+    {
+        var cubes = line.Split(":")[1].Split(";");
+        bool valid = true;
+        foreach (var cube in cubes)
+        {
+            var result = Eval(cube);
+            for (int i = 0; i < 3; i++)
+            {
+                if (result[i] > checker[i])
+                {
+                    valid = false;
+                }
+            }
+        }
+        if (valid)
+        {
+            total+= index + 1;
+        }
+    }
+    return total;
+}
+
+int GetGames2()
+{
+    var total = 0;
+    foreach (var (line, index) in input.Select((line, index) => (line, index)))
+    {
+        var subtotal = 1;
+        var cubes = line.Split(":")[1].Split(";");
+        var max = new int[3];
+        foreach (var cube in cubes)
+        {
+            var result = Eval(cube);
+            for (var i = 0; i < 3; i++)
+            {
+                if (result[i] > max[i])
+                {
+                    max[i] = result[i];
+                }
+            }
+        }
+        for (var i = 0; i < 3; i++)
+        {
+            if (max[i] is not 0)
+            {
+                subtotal *= max[i]; 
+            }
+        }
+
+        total += subtotal;
+    }
+    return total;
+}
+
+int[] Eval(string cube)
+{
+    var numbers = new Dictionary<string, int>
+    {
+        { "red", 0 },
+        { "green", 1 },
+        { "blue", 2 }
+    };
+    int[] result = new int[3];
+    var color = cube.Split(",");
+    foreach (var c in color)
+    {
+        var num = int.Parse(c.Split(" ")[1]);
+        var pos = numbers.FirstOrDefault(pair => c.Split(" ")[2].Equals(pair.Key)).Value;
+        result[pos] = num;
+    }
+   
+    return result;
+}
+
