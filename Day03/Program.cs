edd73d716394ed080fb3f85913c488f4fcac212a
Juan Oh <jioh1982@gmail.com>
2025-05-14 17:41:37 -0400

    feat: Day03 init

---
+++ Day03/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine($"Running directory: {Directory.GetCurrentDirectory()}");
+
+string[] input = File.ReadAllLines($"{Directory.GetCurrentDirectory()}/test.txt");
+
+input.ToList().ForEach(Console.WriteLine);
