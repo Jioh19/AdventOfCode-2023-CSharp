3478ea39e4d8876dacb955a1157006e07848cba7
Juan Oh <jioh1982@gmail.com>
2025-05-15 12:04:11 -0400

    feat(day03): solve part 2 - calculate gear ratio sum

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -2,6 +2,92 @@
 
 Console.WriteLine($"Running directory: {Directory.GetCurrentDirectory()}");
 
-string[] input = File.ReadAllLines($"{Directory.GetCurrentDirectory()}/test.txt");
+var input = File.ReadAllLines($"{Directory.GetCurrentDirectory()}/input.txt");
+
+
+var coords = input.SelectMany((row, y) =>
+        row.Select((character, x) => (character, x, y)))
+    .Where(item => !char.IsDigit(item.character) && item.character != '.')
+    .Select(item => (item.x, item.y))
+    .ToList();
+
+var coords2 = input.SelectMany((row, y) =>
+        row.Select((character, x) => (character, x, y)))
+    .Where(item => item.character == '*')
+    .Select(item => (item.x, item.y))
+    .ToList();
 
 input.ToList().ForEach(Console.WriteLine);
+coords2.ForEach(item => Console.WriteLine(item.x + " " + item.y));
+//Console.WriteLine(Part1(input));
+Console.WriteLine(Part2(input));
+return;
+
+string GetStrNumber(string[] grid, int x, int y)
+{
+    if (x < 0 || x >= grid.Length || !char.IsDigit(grid[y][x]))
+    {
+        return "";
+    }
+
+    var num = grid[y][x].ToString();
+    var rowChars = grid[y].ToCharArray();
+    rowChars[x] = '.';
+    grid[y] = new string(rowChars);
+    return GetStrNumber(grid, x - 1, y) + num + GetStrNumber(grid, x + 1, y);
+}
+
+int Part1(string[] grid)
+{
+    var total = 0;
+    foreach (var coord in coords)
+    {
+        for (var i = -1; i <= 1; i++)
+        {
+            for (var j = -1; j <= 1; j++)
+            {
+                if (i + coord.x < 0 || i + coord.x >= grid[0].Length || j + coord.y < 0 || j + coord.y >= grid.Length ||
+                    !char.IsDigit(grid[j + coord.y][i + coord.x]))
+                {
+                    continue;
+                }
+
+                total += int.Parse(GetStrNumber(grid, i + coord.x, j + coord.y));
+            }
+        }
+    }
+
+    return total;
+}
+
+int Part2(string[] grid)
+{
+    var total = 0;
+    foreach (var coord in coords2)
+    {
+        var num1 = 0;
+        for (var i = -1; i <= 1; i++)
+        {
+            for (var j = -1; j <= 1; j++)
+            {
+                if (i + coord.x < 0 || i + coord.x >= grid[0].Length || j + coord.y < 0 || j + coord.y >= grid.Length ||
+                    !char.IsDigit(grid[j + coord.y][i + coord.x]))
+                {
+                    continue;
+                }
+
+                if (num1 == 0)
+                {
+                    num1 = int.Parse(GetStrNumber(grid, i + coord.x, j + coord.y));
+                }
+                else
+                {
+                    var num2 = int.Parse(GetStrNumber(grid, i + coord.x, j + coord.y));
+                    total += num1 * num2;
+                }
+            }
+        }
+    }
+
+    return total;
+}
