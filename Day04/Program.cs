c334411ace378790e48e11681ffb0035b89c1452
Juan Oh <jioh1982@gmail.com>
2025-05-16 19:42:37 -0400

    feat(day04): Complete Part 1 solution

---
+++ Day04/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine($"Running directory: {Directory.GetCurrentDirectory()}");
+
+var input = File.ReadAllLines($"{Directory.GetCurrentDirectory()}/input.txt");
+
+var cards = input.ToList()
+    .Select(row => row.Split(":")[1].Split("|")[0].Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray())
+    .ToList();
+
+var winners = input.ToList()
+    .Select(row => row.Split(":")[1].Split("|")[1].Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray())
+    .ToList();
+
+Console.WriteLine(Part1(cards, winners));
+
+
+int Part1(List<int[]> cards, List<int[]> winners)
+{
+    var total = 0;
+    var won = cards
+        .Select((game, index) => game.Intersect(winners[index]).ToArray())
+        .ToList();
+    foreach (var numbers in won)
+    {
+        total += (int)Math.Pow(2, numbers.Length - 1);
+    }
+    
+    return total;
+}
