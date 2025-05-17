f9ec43785b3874565e37e02ce23e3437ab380203
Juan Oh <jioh1982@gmail.com>
2025-05-16 20:33:31 -0400

    feat(day04): Complete Part 2 solution

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -16,10 +16,12 @@ var winners = input.ToList()
         .ToArray())
     .ToList();
 
-Console.WriteLine(Part1(cards, winners));
+Console.WriteLine(Part1());
+Console.WriteLine(Part2());
+return;
 
 
-int Part1(List<int[]> cards, List<int[]> winners)
+int Part1()
 {
     var total = 0;
     var won = cards
@@ -29,6 +31,26 @@ int Part1(List<int[]> cards, List<int[]> winners)
     {
         total += (int)Math.Pow(2, numbers.Length - 1);
     }
-    
+
     return total;
+}
+
+int Part2()
+{
+    var won = cards
+        .Select((game, index) => game.Intersect(winners[index]).ToArray())
+        .ToList();
+    int[] tickets = Enumerable.Repeat(1, won.Count).ToArray();
+    var index = 0;
+    foreach (var numbers in won)
+    {
+        for (var i = 1; i <= numbers.Length && i + index < won.Count; i++)
+        {
+            tickets[i + index] += tickets[index];
+        }
+
+        index++;
+    }
+    
+    return tickets.Sum();
 }
