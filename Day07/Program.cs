b5cdf38e5796fed7ca3dd61d5f561bcc07d200bb
Juan Oh <jioh1982@gmail.com>
2025-05-25 03:04:11 -0400

    feat(Day07): Part 2 done

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -5,6 +5,7 @@ Console.WriteLine($"Running directory: {Directory.GetCurrentDirectory()}");
 var input = File.ReadAllLines($"{Directory.GetCurrentDirectory()}/input.txt");
 
 Part1();
+Part2();
 return;
 
 int GetValue(string line)
@@ -40,6 +41,43 @@ int GetValue(string line)
     };
 }
 
+int GetValue2(string line)
+{
+    var first = 0;
+    var second = 0;
+    var characters = line.Split(" ")[0].Distinct().ToArray();
+    //Console.WriteLine(string.Join(" ", characters));
+    foreach (var t in characters)
+    {
+        var count = line.Split(" ")[0].Count(c => c == t);
+        if (t is not 'J')
+        {
+            count += line.Split(" ")[0].Count(c => c == 'J');
+        }
+        
+        if (first < count)
+        {
+            second = first - line.Split(" ")[0].Count(c => c == 'J');
+            first = count;
+        }
+        else if (second < count)
+        {
+            second = count - line.Split(" ")[0].Count(c => c == 'J');
+        }
+
+    }
+
+    return (first, second) switch
+    {
+        (5, _) => 7,
+        (4, _) => 6,
+        (3, 2) => 5,
+        (3, _) => 4,
+        (2, 2) => 3,
+        (2, _) => 2,
+        _ => 1
+    };
+}
 List<int> GetHigh(string line)
 {
     if (string.IsNullOrEmpty(line))
@@ -58,15 +96,48 @@ List<int> GetHigh(string line)
     }).ToList();
 }
 
+List<int> GetHigh2(string line)
+{
+    if (string.IsNullOrEmpty(line))
+        return [];
+
+    var hand = line.Split(" ")[0];
+    return hand.Select(c => c switch
+    {
+        >= '2' and <= '9' => int.Parse(c.ToString()),
+        'T' => 10,
+        'J' => 1,
+        'Q' => 12,
+        'K' => 13,
+        'A' => 14,
+        _ => 0
+    }).ToList();
+}
+
 void Part1()
 {
-    var orderedHands = input.OrderBy(GetValue).ThenBy(line => GetHigh(line), new ListComparer());
+    var orderedHands = input.OrderBy(GetValue).ThenBy(GetHigh, new ListComparer());
 
     var total = 0;
 
     foreach (var (line, index) in orderedHands.Select((value, i) => (value, i)))
     {
        // Console.WriteLine($"{line} index: {index} value: {GetValue(line)}");
+        total += int.Parse(line.Split(" ")[1]) * (index + 1);
+    }
+
+    Console.WriteLine($"Total: {total}");
+}
+
+void Part2()
+{
+    var orderedHands = input.OrderBy(GetValue2).ThenBy(GetHigh2, new ListComparer());
+
+    var total = 0;
+
+    foreach (var (line, index) in orderedHands.Select((value, i) => (value, i)))
+    {
+        // Console.WriteLine($"{line} index: {index} value: {GetValue(line)}");
         total += int.Parse(line.Split(" ")[1]) * (index + 1);
     }
 
