4190e6e9c0b25721ce28fac186e329c20d7e04f3
Juan Oh <jioh1982@gmail.com>
2025-05-25 03:39:48 -0400

    feat(Day08): Part1 init

---
+++ Day08/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine($"Running directory: {Directory.GetCurrentDirectory()}");
+
+var input = File.ReadAllLines($"{Directory.GetCurrentDirectory()}/test.txt");
+
+input.ToList().ForEach(Console.WriteLine);
+
+internal class Node
+{
+    public string Key;
+    Node left, right;
+
+    public Node(string key, Node left, Node right)
+    {
+        Key = key;
+        this.left = left;
+        this.right = right;
+    }
+}
