aef156be005a5fa2bdd4cdf29bb0d0e1ed2987e9
Juan Oh <jioh1982@gmail.com>
2025-05-27 02:10:01 -0400

    feat(Day09): Init

---
+++ Day09/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine($"Running directory: {Directory.GetCurrentDirectory()}");
+
+var input = File.ReadAllLines($"{Directory.GetCurrentDirectory()}/test.txt");
+
+input.ToList().ForEach(Console.WriteLine);
+
+var line = input.ToList()[0].Split(' ').ToList();
+
+var result = line.ToList().Select(int.Parse).Skip(1)
+    .Aggregate(
+        new { Prev = int.Parse(line.ToList()[0]), Diffs = new List<int>() },
+        (acc, curr) => new {
+            Prev = curr,
+            Diffs = new List<int>(acc.Diffs) { curr - acc.Prev }
+        },
+        acc => acc.Diffs
+    );
+
+result.ToList().ForEach(Console.WriteLine);
