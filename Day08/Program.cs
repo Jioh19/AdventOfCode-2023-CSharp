31036d39042855dc6df6c7a58cf6cc52e3045735
Juan Oh <jioh1982@gmail.com>
2025-05-26 20:15:56 -0400

    feat(Day08): Part 1 done

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -2,19 +2,68 @@
 
 Console.WriteLine($"Running directory: {Directory.GetCurrentDirectory()}");
 
-var input = File.ReadAllLines($"{Directory.GetCurrentDirectory()}/test.txt");
+var input = File.ReadAllLines($"{Directory.GetCurrentDirectory()}/input.txt");
 
-input.ToList().ForEach(Console.WriteLine);
+//input[2..].ToList().ForEach(Console.WriteLine);
 
-internal class Node
+var nav = input[0];
+
+var map = new Dictionary<string, Node>();
+
+InsertData();
+
+// Console.WriteLine(nav);
+// foreach (var keyValuePair in map)
+// {
+//     Console.WriteLine($"{keyValuePair.Key} - {keyValuePair.Value.Left?.Key} - {keyValuePair.Value.Right?.Key}");
+// }
+
+Console.WriteLine(Part1());
+return;
+
+void InsertData()
 {
-    public string Key;
-    Node left, right;
-
-    public Node(string key, Node left, Node right)
+    input[2..].ToList().ForEach(str =>
     {
-        Key = key;
-        this.left = left;
-        this.right = right;
+        var key = str.Split(" = ")[0];
+        var node = AddGetNode(key);
+        var next = str.Split(" = ")[1];
+        var left = next.Split(", ")[0].Replace("(", "");
+        var right = next.Split(", ")[1].Replace(")", "");
+        node.Left = AddGetNode(left);
+        node.Right = AddGetNode(right);
+        //  Console.WriteLine($"{key} {left} {right}");
+    });
+}
+
+Node AddGetNode(string key)
+{
+    if (map.TryGetValue(key, out var value)) return value;
+    var node = new Node(key);
+    map.Add(node.Key, node);
+
+    return node;
+}
+
+int Part1()
+{
+    var total = 0;
+    var node = map[input[2].Split(" = ")[0]];
+   // Console.WriteLine(node.Key);
+    while (node?.Key is not "ZZZ")
+    {
+        var direction = nav[total % nav.Length];
+        node = direction is 'L' ? node?.Left : node?.Right;
+       // Console.WriteLine(node?.Key );
+        total++;
     }
+
+    return total;
+}
+
+internal class Node(string key)
+{
+    public string Key { get; set; } = key;
+    public Node? Left { get; set; }
+    public Node? Right { get; set; }
 }
